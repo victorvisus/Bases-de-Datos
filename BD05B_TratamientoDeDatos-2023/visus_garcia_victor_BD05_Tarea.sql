ade6aaca65342542e2ba3bb37da7e254ad68a96d
Victor Visús <victor.vxg@gmail.com>
2023-03-03 07:52:46 +0100

    Actualizar visus_garcia_victor_BD05_Tarea.docx, visus_garcia_victor_BD05_Tarea.pdf y visus_garcia_victor_BD05_Tarea.sql

--- BD05B_TratamientoDeDatos-2023/visus_garcia_victor_BD05_Tarea.sql
+++ BD05B_TratamientoDeDatos-2023/visus_garcia_victor_BD05_Tarea.sql
@@ -27,7 +27,14 @@ DELETE FROM alumnado WHERE cod_curso = 3;
 --8.
 INSERT INTO alumnado (nombre, apellidos, sexo, fecha_nac)
     SELECT nombre, apellidos, sexo, fecha_nac FROM alumnado_nuevo;
-    
---86+30
-SELECT COUNT(*) AS num_alumn FROM alumnado;
-SELECT COUNT(*) FROM alumnado_nuevo;
+
+--9
+UPDATE cursos SET max_alumn = (
+    SELECT COUNT(codigo) FROM alumnado WHERE cod_curso = 2
+) WHERE codigo = 2;
+
+--10
+DELETE FROM alumnado WHERE cod_curso IN (
+  SELECT c.codigo FROM cursos c, profesorado p
+  WHERE c.cod_profe = p.codigo AND p.nombre = 'NURIA'
+);
